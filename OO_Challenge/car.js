2edfebb1a2228c997d7d8daccd30815997715f07
aaron-mancini <aaronmancini2301@gmail.com>
2021-07-12 15:04:24 -0400

    Car OO Project

---
+++ OO_Challenge/car.js
@@ -0,0 +1,53 @@
+class Vehicle {
+    constructor(make, model, year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    honk() {
+        return "Beep.";
+    }
+
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}`;
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+    
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+
+    revEngine() {
+        return "VROOM!!!";
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+
+    add(vehicle) {
+        if(this.vehicles.length === 2){
+            return "Sorry, we're full."
+        }
+        if(vehicle instanceof Vehicle){
+            this.vehicles.push(vehicle);
+            return "Vehicle added!"
+        } else {
+            return "Only vehicles are allowed in here!"
+        }
+    }
+}
