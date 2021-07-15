a44c6003676bc16bc47196c3b5e1b8496a960fc8
aaron-mancini <aaronmancini2301@gmail.com>
2021-07-15 14:02:47 -0400

    how the web works

---
+++ How_The_Web_Works_Exercise/app.js
@@ -0,0 +1,38 @@
+// Part One: Solidify Terminology
+
+// What is a HTTP?
+// HTTP is how web browsers and Servers communicate.
+
+// What is a URL?
+// A URL is a unique address of a resource or page on the Web.
+
+// What is DNS?
+// DNS stands for Domain Name System. It will take a url and "translate" into the 
+// corresponding ip address.
+
+// What is a query string?
+// It is part of the URL that has keys and value pairs. They are added to a url
+// to give more information to the website server when you enter a url.
+
+// What are two HTTP verbs and how are they different?
+// The most common verbs are GET and POST. A GET request will request information 
+// from some server on the internet. A POST request will request that some
+// information on the server to be changed or added.
+
+// What is an HTTP request?
+// Information that is sent to a server from your web browser. 
+
+// What is an HTTP response?
+// Information that is sent to your web browser from a server. Usually after a HTTP request.
+
+// What is an HTTP header? Give a couple examples of request and response headers you have seen.
+// A header is line of code that details information when your browser is making a request.
+// It can set the language of the page and give other information like status code, request method, and dates.
+
+// What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+// Your browser will try to obtain the IP address for that URL and make a http request to the server at that
+// address for the information you requested based on your url and other predefined variables. Your browser
+// might have the IP address, if not then it will check your router, then your ISP and lastly it will contact a DNS
+// server if needed. After the address is found your browser will make a http request to the server of that
+// address and the server will collect and organize all the data you asked for and send it to your browser.
+// Your browser will then display that information to you.
