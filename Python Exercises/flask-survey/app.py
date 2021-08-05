fd728cd34a1790392959c5ddd4cfd20e9e6f7528
aaron-mancini <aaronmancini2301@gmail.com>
2021-08-05 11:04:40 -0400

    session

--- Python Exercises/flask-survey/app.py
+++ Python Exercises/flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
@@ -12,6 +12,11 @@ def survey_start():
     responses.clear()
     return render_template('survey.html', title=satisfaction_survey.title, instructions=satisfaction_survey.instructions)
 
+@app.route('/start_survey', methods=['GET', 'POST'])
+def start_survey():
+    session['responses'] = []
+    return redirect('/questions/0')
+
 @app.route('/questions/<int:num>')
 def questions(num):
     if num != len(responses):
@@ -22,6 +27,9 @@ def questions(num):
 
 @app.route('/answers/<int:num>', methods=["GET"])
 def answers(num):
+    response = session['responses']
+    response.append(request.args['answer'])
+    session['responses'] = response
     answer = request.args['answer']
     responses.append(answer)
     if len(responses) == len(satisfaction_survey.questions):
